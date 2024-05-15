06d689c3fb4e0d3480dbd9f2098304b41901e6d8
mo-adel-tronic <mo.adel.developer@gmail.com>
2024-05-15 17:15:23 +0300

    init light ex

--- lib/blocs/cubit/cubit/product_state.dart
+++
@@ -1,9 +0,0 @@
-part of 'product_cubit.dart';
-
-sealed class ProductState {}
-
-final class ProductInitial extends ProductState {}
-final class ProductLoaded extends ProductState {
-  final List<Product> products;
-  ProductLoaded(this.products);
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,50 @@
 import 'package:flutter/material.dart';
-import 'package:bloc_ex/app_router.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'light_bloc.dart';
 
 void main() {
-  runApp(MyApp(appRouter: AppRouter(),));
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final AppRouter appRouter;
-  const MyApp({
-    super.key,
-    required this.appRouter,
-  });
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      onGenerateRoute: appRouter.generateRoute,
+      title: 'Flutter Bloc Example',
+      home: BlocProvider(
+        create: (context) => LightBloc(),
+        child: LightPage(),
+      ),
+    );
+  }
+}
+
+class LightPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Flutter Bloc Example')),
+      body: Center(
+        child: BlocBuilder<LightBloc, LightState>(
+          builder: (context, state) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  state is LightOn ? Icons.lightbulb : Icons.lightbulb_outline,
+                  size: 100,
+                  color: state is LightOn ? Colors.yellow : Colors.grey,
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () => context.read<LightBloc>().add(ToggleLight()),
+                  child: Text(state is LightOn ? 'Turn Off' : 'Turn On'),
+                ),
+              ],
+            );
+          },
+        ),
+      ),
     );
   }
 }
